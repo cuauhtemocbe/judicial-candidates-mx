4e61c5a76507aa94c83da2ad6c769b8a364d30d0
Kuautli <cuauhtemocbe@gmail.com>
2025-05-19 00:51:04 +0000

    Add candidates for green ballot

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 from flask import Blueprint, render_template
-from .data import analistas_data, resumen_candidatos, fuentes
+from .data import analistas_data, resumen_candidatos, fuentes, candidatos_tdj
 
 bp = Blueprint("main", __name__)
 
@@ -28,3 +28,8 @@ def index():
 @bp.route("/acerca")
 def acerca():
     return render_template("acerca.html")
+
+
+@bp.route("/boleta-verde")
+def boleta_verde():
+    return render_template("boleta_verde.html", candidatos_tdj=candidatos_tdj)
